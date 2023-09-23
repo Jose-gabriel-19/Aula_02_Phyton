f588ded3d10ad3d15a9d071d0798606d8f020da5
Jose-gabriel-19 <145858759+Jose-gabriel-19@users.noreply.github.com>
2023-09-23 17:20:46 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+# print('oi')
+# numero = 10
+# numero2 = 2
+# numero3 = numero + numero2
+# print(numero3)
+
+# if numero3 == numero2:
+#   print('é igual')
+# else: 
+#   print("não é igual")
+
+# entrada = input("digite seu nome\n->")
+# print('oi,tudo bem?',entrada)
+# print("calculadora de soma ")
+# n1 = float(input('digite um numero\n->'))
+# n2= float(input("digite o número 2\n->"))
+# n3= n1+n2
+# print('o resultado é:\n', n3)
+
+print('\n')
+
+print('calculadora:⇩\n')
+print('\n')
+n1 = float(input("digite um número:\n⇨"))
+n2 = float(input("digite outro número:\n⇨"))
+n3= n1+n2
+print('oresultado é:', n3)
+print('\n')
+print('<-----<=+=>----->')
+print('\n')
+
+
+nome = input('digite seu nome:⇩\n⇨')
+print('\n')
+print('Oie tudo bem', nome,'?')
+print('\n')
+print('<-----<=+=>----->')
+print('\n')
+
+print('calculadora de multiplicação⇩\n')
+print('\n')
+n1 = float(input("digite um número:\n⇨"))
+n2 = float(input("digite outro número:\n⇨"))
+n3= n1*n2
+print('o resultado é:', n3)
